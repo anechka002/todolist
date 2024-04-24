e4524b9cb6813b17b69c7d185003a74604bd9a66
Anna <blinova.anneta@gmail.com>
2024-04-24 21:50:05 +0200

    update component

--- src/components/todolist/TodoList.tsx
+++ src/components/todolist/TodoList.tsx
@@ -1,21 +1,24 @@
 import React, { KeyboardEvent, ChangeEvent, useState } from 'react'
-import { FilterValuesType, TaskType } from '../../App'
+import { FilterValuesType, TaskStateType, TaskType, TodolistType } from '../../App'
 import Button from '../button/Button'
 import s from '../button/Button.module.css'
 
 type TodoListPropsType = {
+  todolistId: string
   title: string
   tasks: TaskType[]
   date?: string
-  removeTask: (taskId: string) => void
-  changeFilter: (value: FilterValuesType) => void
+  removeTask: (todolistId: string, taskId: string) => void
+  changeFilter: (todolistId: string, value: FilterValuesType) => void
   deleteAllTasks: (value: FilterValuesType) => void
-  addTask: (title: string) => void
-  changeStatus: (taskId: string, isDone: boolean) => void
+  addTask: (todolistId: string, title: string) => void
+  changeStatus: (todolistId: string, taskId: string, isDone: boolean) => void
   filter: FilterValuesType
+  removeTodolist: (todolistId: string) => void
 }
 
 function TodoList({
+  todolistId,
   title, 
   tasks, 
   date, 
@@ -24,8 +27,19 @@ function TodoList({
   deleteAllTasks, 
   addTask,
   changeStatus,
-  filter
+  filter, 
+  removeTodolist
 }: TodoListPropsType) {
+
+  let tasksForTodolist = tasks
+  if (filter === 'active') {
+    tasksForTodolist = tasks.filter(task => !task.isDone)
+  }
+
+  if (filter === 'completed') {
+    tasksForTodolist = tasks.filter(task => task.isDone)
+    console.log(tasksForTodolist)
+  }
 
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const [taskInputError, setTaskInputError] = useState<string | null>(null)
@@ -45,34 +59,39 @@ function TodoList({
     }
   }
 
-  const deleteAllTasksHandler = (value: FilterValuesType) => {
-    changeFilter('delete')
-    deleteAllTasks(value)
-  }
+  // const deleteAllTasksHandler = (value: FilterValuesType) => {
+  //   changeFilter('delete')
+  //   deleteAllTasks(value)
+  // }
 
   const onClickAddTaskHandler = () => {
     const trimmedTaskTitle = newTaskTitle.trim()
     if(trimmedTaskTitle) {
-      addTask(trimmedTaskTitle)     
+      addTask(todolistId,trimmedTaskTitle)     
     } else {
       setTaskInputError('Title is required')
     }
     setNewTaskTitle('')
   }
 
-  const changeFilterHandler = (value: FilterValuesType) => {
-    changeFilter(value)
+  const changeFilterHandler = (todolistId: string, value: FilterValuesType) => {
+    changeFilter(todolistId, value)
   }
 
   const removeTaskHandler = (taskId: string) => {
-    removeTask(taskId)
+    removeTask(todolistId,taskId)
   }
 
-  
+  const removeTodolistHandler = () => {
+    removeTodolist(todolistId)
+  }
 
   return (
     <div className='todolist'>
-      <h3>{title}</h3>
+      <h3>
+        {title}
+        <button onClick={removeTodolistHandler}>x</button>
+      </h3>
       <div>
         <input 
           className={taskInputError ? 'task-input-error' : ''} 
@@ -85,19 +104,19 @@ function TodoList({
         {taskInputError && <div className='task-input-error-message'>{taskInputError}</div>}
       </div>
 
-      {tasks.length === 0 ? (
+      {tasksForTodolist.length === 0 ? (
         <p>Тасок нет :(</p>
       ) : (
       <ul>
 
-        {tasks.map( (t) => {
+        {tasksForTodolist.map( (t) => {
           // const removeTaskHandler = () => {
           //   removeTask(t.id)
           // }
 
           const changeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
             // console.log(e.currentTarget.checked)
-            changeStatus(t.id, e.currentTarget.checked)
+            changeStatus(todolistId, t.id, e.currentTarget.checked)
           }
         
           return (
@@ -112,12 +131,12 @@ function TodoList({
       </ul>
       )}
 
-      <Button className={filter === 'delete' ? s.filterBtnActive : ''} callBack={()=>deleteAllTasksHandler('delete')} title={'DELETE ALL TASKS'}/>
+      {/* <Button className={filter === 'delete' ? s.filterBtnActive : ''} callBack={() => {deleteAllTasksHandler('delete')}} title={'DELETE ALL TASKS'}/> */}
 
       <div className='btn'>
-        <Button className={filter === 'all' ? s.filterBtnActive : ''} callBack={() => {changeFilterHandler('all')}} title={'All'}/>
-        <Button className={filter === 'active' ? s.filterBtnActive : ''} callBack={() => {changeFilterHandler('active')}} title={'Active'}/>
-        <Button className={filter === 'completed' ? s.filterBtnActive : ''} callBack={() => {changeFilterHandler('completed')}} title={'Completed'}/>
+        <Button className={filter === 'all' ? s.filterBtnActive : ''} callBack={() => {changeFilterHandler(todolistId, 'all')}} title={'All'}/>
+        <Button className={filter === 'active' ? s.filterBtnActive : ''} callBack={() => {changeFilterHandler(todolistId, 'active')}} title={'Active'}/>
+        <Button className={filter === 'completed' ? s.filterBtnActive : ''} callBack={() => {changeFilterHandler(todolistId, 'completed')}} title={'Completed'}/>
       </div>
       <div>{date}</div> 
     </div>
