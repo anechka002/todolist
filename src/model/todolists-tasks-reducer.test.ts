3952d6da547b6c0ce1dd146d4e28f22519637ecd
Anna <blinova.anneta@gmail.com>
2024-06-12 10:01:18 +0200

    Test todolists-tasks-reducer

---
+++ src/model/todolists-tasks-reducer.test.ts
@@ -0,0 +1,20 @@
+import { TasksStateType, TodolistsType } from "../App"
+import { tasksReducer } from "./task-reducer"
+import { addTodoListAC, todoListsReducer } from "./todolist-reducer"
+
+test('ids should be equals', () => {
+  const startTasksState: TasksStateType = {}
+  const startTodolistsState: Array<TodolistsType> = []
+
+  const action = addTodoListAC('new todolist')
+
+  const endTasksState = tasksReducer(startTasksState, action)
+  const endTodolistsState = todoListsReducer(startTodolistsState, action)
+
+  const keys = Object.keys(endTasksState)
+  const idFromTasks = keys[0]
+  const idFromTodolists = endTodolistsState[0].id
+
+  expect(idFromTasks).toBe(action.todolistId)
+  expect(idFromTodolists).toBe(action.todolistId)
+})
