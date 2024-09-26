46e9658dfc1e5af536a97c7ad6d7726ddc826fcb
Anna <blinova.anneta@gmail.com>
2024-09-26 14:25:04 +0200

    commit

--- src/model/state/store.ts
+++ src/model/state/store.ts
@@ -1,6 +1,12 @@
-import { combineReducers, legacy_createStore } from "redux";
+import { combineReducers, compose, legacy_createStore } from "redux";
 import { tasksReducer } from "../task-reducer";
 import { todoListsReducer } from "../todolist-reducer";
+
+declare global {
+  interface Window {
+      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -8,8 +14,11 @@ const rootReducer = combineReducers({
   tasks: tasksReducer,
   todolists: todoListsReducer
 })
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // непосредственно создаём store
-export const store = legacy_createStore(rootReducer);
+export const store = legacy_createStore(rootReducer, {}, composeEnhancers());
 // определить автоматически тип всего объекта состояния
 // export type AppRootStateType = ReturnType<typeof rootReducer>
 
