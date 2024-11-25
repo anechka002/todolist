a0d13be5dac38d87b6d6044826e901033eee7ffa
Anna <blinova.anneta@gmail.com>
2024-11-25 12:50:59 +0100

    get the tasks through useSelector

--- src/features/todolistsList/ui/todolist/tasks/Tasks.tsx
+++ src/features/todolistsList/ui/todolist/tasks/Tasks.tsx
@@ -1,35 +1,34 @@
-import { List } from '@mui/material';
-import { Task } from './task/Task'
-import { TodoListDomainType } from '../../../bll/todolist-reducer';
-import { useAppSelector } from 'common/hooks/useAppSelector';
-import { selectTasks } from 'app/appSelectors';
-import { RequestStatusType } from 'app/app-reducer';
-import { TaskStatuses } from 'features/todolistsList/lib/enum';
+import { List } from "@mui/material"
+import { Task } from "./task/Task"
+import { TodoListDomainType } from "../../../bll/todolist-reducer"
+import { useAppSelector } from "common/hooks/useAppSelector"
+import { selectTasks } from "app/appSelectors"
+import { RequestStatusType } from "app/bll/app-reducer"
+import { TaskStatuses } from "features/todolistsList/lib/enum"
 
 type Props = {
   todolist: TodoListDomainType
 }
 
-export const Tasks = ({todolist}: Props) => {
-
+export const Tasks = ({ todolist }: Props) => {
   // const { id, title, filter, addedDate, order, entityStatus } = todolist;
 
-  let tasks = useAppSelector(selectTasks);
+  let tasks = useAppSelector(selectTasks)
   // let tasks = useAppSelector((state) => state.tasks[id]);
 
   const allTodolistTasks = tasks[todolist.id] || []
-  
+
   let tasksForTodolist = allTodolistTasks.map((task) => ({
     ...task,
-    entityStatus: 'idle' as RequestStatusType, // Добавляем свойство entityStatus, если оно отсутствует
-  }));
+    entityStatus: "idle" as RequestStatusType, // Добавляем свойство entityStatus, если оно отсутствует
+  }))
 
-    if (todolist.filter === 'active') {
-      tasksForTodolist = tasksForTodolist.filter((el) => el.status === TaskStatuses.New);
-    }
-    if (todolist.filter === 'completed') {
-      tasksForTodolist = tasksForTodolist.filter((el) => el.status === TaskStatuses.Completed);
-    }
+  if (todolist.filter === "active") {
+    tasksForTodolist = tasksForTodolist.filter((el) => el.status === TaskStatuses.New)
+  }
+  if (todolist.filter === "completed") {
+    tasksForTodolist = tasksForTodolist.filter((el) => el.status === TaskStatuses.Completed)
+  }
 
   return (
     <div>
