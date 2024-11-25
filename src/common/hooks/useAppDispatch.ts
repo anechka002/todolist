a70c6439df1ea8bef47ebd3aaa8a13dcc549a587
Anna <blinova.anneta@gmail.com>
2024-11-25 12:46:10 +0100

    update imports

--- src/common/hooks/useAppDispatch.ts
+++ src/common/hooks/useAppDispatch.ts
@@ -1,6 +1,6 @@
 import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../app/store';
+import { AppDispatchType } from "../../app/store"
 
 // Хук для типизированного использования dispatch
 // export const useAppDispatch = useDispatch<AppDispatchType>; //для санок
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppDispatch = useDispatch.withTypes<AppDispatchType>();
--- src/common/theme/theme.ts
+++ src/common/theme/theme.ts
@@ -1,19 +1,20 @@
-import { createTheme } from "@mui/material/styles";
-import { RequestThemeType } from "../../app/app-reducer";
+import { createTheme } from "@mui/material/styles"
+import { RequestThemeType } from "../../app/bll/app-reducer"
 
-export const getTheme = (themeMode: RequestThemeType) => createTheme({
-  palette: {
-    mode: themeMode,
-    // mode: themeMode === 'light' ? 'light' : 'dark',
-    primary: {
-      main: '#1565c0',
-      contrastText: 'white',
+export const getTheme = (themeMode: RequestThemeType) =>
+  createTheme({
+    palette: {
+      mode: themeMode,
+      // mode: themeMode === 'light' ? 'light' : 'dark',
+      primary: {
+        main: "#1565c0",
+        contrastText: "white",
+      },
+      secondary: {
+        light: "#5d67f7",
+        main: "#1831c3",
+        dark: "#001c5d",
+        contrastText: "#fff",
+      },
     },
-    secondary: {
-      light: '#5d67f7',
-      main: '#1831c3',
-      dark: '#001c5d',
-      contrastText: '#fff',
-    },
-  },
-});
+  })
