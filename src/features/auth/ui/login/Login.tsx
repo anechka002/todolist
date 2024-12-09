bdefc5e10973c2c47e753f3abfd54b9ccf1563a6
Anna <blinova.anneta@gmail.com>
2024-12-09 16:19:15 +0100

    Data retrieval React Hook Form

--- src/features/auth/ui/login/Login.tsx
+++ src/features/auth/ui/login/Login.tsx
@@ -9,10 +9,32 @@ import TextField from '@mui/material/TextField'
 import { selectThemeMode } from '../../../../app/appSelectors'
 import { getTheme } from 'common/theme'
 import { useAppSelector } from 'common/hooks'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import s from './Login.module.css'
+
+type Inputs = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
 
 export const Login = () => {
   const themeMode = useAppSelector(selectThemeMode)
   const theme = getTheme(themeMode)
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<Inputs>({defaultValues: { email: '', password: 'MasteR!123', rememberMe: false },})
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+    reset()
+  }
 
   return (
     <Grid container justifyContent={'center'}>
@@ -38,14 +60,53 @@ export const Login = () => {
               <b>Password:</b> free
             </p>
           </FormLabel>
-          <FormGroup>
-            <TextField label="Email" margin="normal" />
-            <TextField type="password" label="Password" margin="normal" />
-            <FormControlLabel label={'Remember me'} control={<Checkbox />} />
-            <Button type={'submit'} variant={'contained'} color={'primary'}>
-              Login
-            </Button>
-          </FormGroup>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormGroup>
+              <TextField 
+                label="Email" 
+                margin="normal" 
+                {...register('email', {
+                  required: 'Email is required',
+                  pattern: {
+                    value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                    message: 'Incorrect email address',
+                  }
+                })} 
+              />
+
+              {errors.email && <span className={s.errorMessage}>{errors.email.message}</span>}
+
+              <TextField 
+                type="password" 
+                label="Password" 
+                margin="normal" 
+                {...register('password', {
+                  required: 'Password is required',
+                  minLength: {
+                    value: 3,
+                    message: 'Password must be at least 3 characters long'
+                  }
+                })} 
+              />
+
+              {errors.password && <span className={s.errorMessage}>{errors.password.message}</span>}
+
+              <FormControlLabel
+                label={'Remember me'}
+                control={
+                  <Controller
+                    name={'rememberMe'}
+                    control={control}
+                    render={({ field: { value, ...rest } }) => <Checkbox {...rest} checked={value} />}
+                  />
+                }
+              />
+
+              <Button type={'submit'} variant={'contained'} color={'primary'}>
+                Login
+              </Button>
+            </FormGroup>
+          </form>
         </FormControl>
       </Grid>
     </Grid>
