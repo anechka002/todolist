2b1519a2abe375840ea23acf6a60913ca806bc2f
Anna <blinova.anneta@gmail.com>
2025-01-22 09:47:10 +0100

    Cache Management. Tasks filtering

--- src/features/todolistsList/ui/todolist/filterTasksButton/FilterTasksButton.tsx
+++ src/features/todolistsList/ui/todolist/filterTasksButton/FilterTasksButton.tsx
@@ -3,7 +3,8 @@ import { Box, Button } from "@mui/material"
 import { filterButtonsContainerSx } from "./FilterTasksButton.styles"
 import { useAppDispatch } from "common/hooks/useAppDispatch"
 import { ButtonWithMemo } from "common/components"
-import { changeTodoListFilter, TodoListDomainType } from "features/todolistsList/bll/todolistsSlice"
+import { FilterValuesType, TodoListDomainType } from "features/todolistsList/bll/todolistsSlice"
+import { todoListsAPI } from "features/todolistsList/api/todolists-api"
 
 type Props = {
   todolist: TodoListDomainType
@@ -14,21 +15,27 @@ export const FilterTasksButton = ({ todolist }: Props) => {
 
   const dispatch = useAppDispatch()
 
-  const onAllClickHandler = useCallback(() => {
-    dispatch(changeTodoListFilter({todolistId: id, filter: "all"}))
-  }, [dispatch])
-  const onActiveClickHandler = useCallback(() => {
-    dispatch(changeTodoListFilter({todolistId: id, filter: "active"}))
-  }, [dispatch])
-  const onCompletedClickHandler = useCallback(() => {
-    dispatch(changeTodoListFilter({todolistId: id, filter: "completed"}))
-  }, [dispatch])
+  const changeFilterTasksHandler = (filter: FilterValuesType) => {
+    dispatch(
+      todoListsAPI.util.updateQueryData('getTodoLists', undefined, (state) => {
+        const todolist = state.find((tl) => tl.id === id)
+        if (todolist) {
+          todolist.filter = filter
+        }
+      })
+    )
+  }
+
+// можно так передавать данные
+  const onCompletedClickHandler = () => {
+    changeFilterTasksHandler("completed")
+  }
 
   return (
     <Box sx={filterButtonsContainerSx}>
       <Button
         variant={filter === "all" ? "outlined" : "text"}
-        onClick={onAllClickHandler}
+        onClick={() => changeFilterTasksHandler('all')}
         color={"inherit"}
         disabled={entityStatus === "loading"}
       >
@@ -36,7 +43,7 @@ export const FilterTasksButton = ({ todolist }: Props) => {
       </Button>
       <Button
         variant={filter === "active" ? "outlined" : "text"}
-        onClick={onActiveClickHandler}
+        onClick={() => changeFilterTasksHandler('active')}
         color={"primary"}
         disabled={entityStatus === "loading"}
       >
